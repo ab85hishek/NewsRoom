eabd2afb17ee2d48de2432a7881ace94505b5c90
ab85hishek <ab85hishek@gmail.com>
2022-08-12 23:28:03 +0530

    created basic news app by using xhr and news api

---
+++ js/app.js
@@ -0,0 +1,38 @@
+console.log("NewsRoom");
+
+
+
+let myNews = document.getElementById("myNews");
+let myApi = "52d898902c18456ea25f132dc3c3b211";
+let source = "bbc-news"
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${myApi}`, true);
+
+xhr.onload = function () {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    console.log(articles);
+    let html = "";
+    articles.forEach(function (element,index) {
+        html += `<div class="accordion-item">
+        <h2 class="accordion-header" id="heading${index}">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapse${index}" aria-expanded="false" aria-controls="collapse${index}">
+                ${element["title"]}
+            </button>
+        </h2>
+        <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}"
+            data-bs-parent="#myNews">
+            <div class="accordion-body">
+               ${element["content"]} <a href=${element["url"]} target="_blank">Click here to read more...</a>
+            </div>
+        </div>
+    </div>`
+    });
+    myNews.innerHTML= html;
+
+}
+
+
+xhr.send();
